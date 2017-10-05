eea84c2290361ee608f4a9aec82787906f454abc
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-10-05 20:04:28 +0530

    commented useless code

--- src/main/java/com/arpit/Amt.java
+++ src/main/java/com/arpit/Amt.java
@@ -205,11 +205,11 @@ public class Amt
         } finally
         {
             log.info("Closing browser");
-            while (driver.getWindowHandles().size() > 1)
+            /*while (driver.getWindowHandles().size() > 1)
             {
                 driver.close();
                 driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
-            }
+            }*/
             //driver.close();
             driver.quit();
             return res;
