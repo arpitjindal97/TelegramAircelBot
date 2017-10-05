c8db978e9d15f0dde0c18f8745936cc629101060
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-10-05 20:11:47 +0530

    semicolon mistake

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 export DISPLAY=:0
 kill -9 $(ps aux | awk '/Xvfb/ { print $2}')
-if [ -f /tmp/.X0-lock ];then rm /tmp/.X0-lock fi
+if [ -f /tmp/.X0-lock ];then rm /tmp/.X0-lock; fi
 Xvfb :0 &
 cd /arpit
 java -jar deployments/*.jar
