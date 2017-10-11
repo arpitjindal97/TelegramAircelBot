50fb733d3d4fcd09d663837ac35a99e1ad9bfecc
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-10-12 00:05:59 +0530

    updated password

--- src/main/java/com/arpit/Database.java
+++ src/main/java/com/arpit/Database.java
@@ -2,7 +2,7 @@ package com.arpit;
 
 public class Database {
     static String amt_username="tpmohkum101",
-                    amt_password="rama@123",
+                    amt_password="abcd@123",
             telegram_token="407376581:AAHn8WqQ2MEalHl-DNAGdloslbvXL866kmw";
 
 }
