cc3adfbd12741515c2c4756e2dc361bab57f461a
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-10-27 18:48:13 +0530

    creds upadted

--- src/main/java/com/arpit/Database.java
+++ src/main/java/com/arpit/Database.java
@@ -1,8 +1,8 @@
 package com.arpit;
 
 public class Database {
-    static String amt_username="tpmohkum101",
-                    amt_password="abcd@123",
+    static String amt_username="tpmukkum190",
+                    amt_password="Oct@1234",
             telegram_token="407376581:AAHn8WqQ2MEalHl-DNAGdloslbvXL866kmw";
 
 }
